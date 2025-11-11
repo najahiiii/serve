ddbb0870617af0bceb9f4f8ba147814e02569501
Ahmad Thoriq Najahi <najahi@zephyrus.id>
2025-11-11 15:57:19 +0700

    fix: set timeout to None instead
    Signed-off-by: Ahmad Thoriq Najahi <najahi@zephyrus.id>

--- serve-cli/src/http.rs
+++ serve-cli/src/http.rs
@@ -2,12 +2,11 @@ use anyhow::{Context, Result};
 use reqwest::Url;
 use reqwest::blocking::{Client, Response};
 use serde::de::DeserializeOwned;
-use std::time::Duration;
 
 pub fn build_client() -> Result<Client> {
     Client::builder()
         .user_agent("serve-cli")
-        .timeout(Duration::from_secs(3600))
+        .timeout(None)
         .build()
         .context("failed to build HTTP client")
 }
